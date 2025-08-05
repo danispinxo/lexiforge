7a7043f9a6083d60ef7426aa48e53c473195e6d2
danispinxo <genericpronoun@gmail.com>
2025-08-05 13:42:50 -0400

    add test factories

---
+++ spec/factories/poems.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :poem do
+    association :source_text
+    title { "Generated Poem" }
+    content { "This is a generated poem\nwith multiple lines\nand interesting words" }
+    technique_used { "cutup" }
+
+    trait :erasure_poem do
+      technique_used { "erasure" }
+      content { "This is an erasure poem with some words     removed and others kept intact." }
+    end
+
+    trait :blackout_poem do
+      technique_used { "blackout" }
+      content { "This is a <span class='blackout-word'>██████</span> poem with <span class='blackout-word'>████</span> words blacked out." }
+    end
+
+    trait :cut_up_poem do
+      technique_used { "cutup" }
+      content { "random words\ncut up lines\nmixed together\nfrom source text" }
+    end
+  end
+end
