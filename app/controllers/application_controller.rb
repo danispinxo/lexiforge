648f8b9b66de628463a66cdbd9b35e410a065d66
danispinxo <genericpronoun@gmail.com>
2025-08-06 12:07:06 -0400

    fix cors for options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.path.start_with?('/api') }
   
   def handle_options_request
+    if request.path.start_with?('/api')
+      response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    end
     head :ok
   end
 end
