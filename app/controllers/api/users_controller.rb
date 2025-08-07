cc04f05d36b562cc4da00037fed99ecafe84b76e
danispinxo <genericpronoun@gmail.com>
2025-08-07 13:35:44 -0400

    same for controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,11 +7,12 @@ class Api::UsersController < ApiController
       if current_api_user
         render json: {
           success: true,
-          user: {
-            id: current_api_user.id,
-            email: current_api_user.email,
-            created_at: current_api_user.created_at
-          }
+          user: UserSerializer.new(current_api_user).as_json
+        }
+      elsif current_admin_user
+        render json: {
+          success: true,
+          user: AdminUserSerializer.new(current_admin_user).as_json
         }
       else
         render json: { success: false, user: nil }
