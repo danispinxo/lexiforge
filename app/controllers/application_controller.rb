ae34a04fd48bbf32b8c9df5055b72b2adc344574
danispinxo <genericpronoun@gmail.com>
2025-08-07 14:21:44 -0400

    separate controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,25 +15,3 @@ class ApplicationController < ActionController::Base
     head :ok
   end
 end
-
-class ApiController < ActionController::Base
-  protect_from_forgery with: :null_session
-  before_action :set_cors_headers
-
-  def handle_options_request
-    head :ok
-  end
-
-  private
-
-  def set_cors_headers
-    allowed_origins = ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3001').split(',')
-    origin = request.headers['Origin']
-
-    response.headers['Access-Control-Allow-Origin'] = origin if allowed_origins.include?(origin)
-
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-  end
-end
