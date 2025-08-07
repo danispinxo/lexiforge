6de4476eb671b2b85da808696c584f0db5383774
danispinxo <genericpronoun@gmail.com>
2025-08-07 12:23:04 -0400

    add user to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_06_163906) do
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema[7.1].define(version: 2025_08_07_161355) do
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
@@ -57,6 +56,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_06_163906) do
     t.datetime "updated_at", null: false
     t.integer "gutenberg_id"
     t.index ["gutenberg_id"], name: "index_source_texts_on_gutenberg_id", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "poems", "source_texts"
