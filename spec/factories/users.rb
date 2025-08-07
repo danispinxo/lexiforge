28c7ae8edd6bb3bed8f5584ebc42844c4d1ee642
danispinxo <genericpronoun@gmail.com>
2025-08-07 13:43:08 -0400

    remove unused traits in factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,17 +3,5 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     password { 'password123' }
     password_confirmation { 'password123' }
-
-    trait :with_poems do
-      after(:create) do |user|
-        create_list(:poem, 3, user: user)
-      end
-    end
-
-    trait :with_source_texts do
-      after(:create) do |user|
-        create_list(:source_text, 2, user: user)
-      end
-    end
   end
 end
