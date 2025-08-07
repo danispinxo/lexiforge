59030045dfacf015091135bd7c0ef0735af215fc
danispinxo <genericpronoun@gmail.com>
2025-08-07 12:17:03 -0400

    better header handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,14 @@ class ApplicationController < ActionController::Base
     if request.path.start_with?('/api')
       allowed_origins = ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3001').split(',')
       origin = request.headers['Origin']
-
-      response.headers['Access-Control-Allow-Origin'] = origin if allowed_origins.include?(origin)
-
+      
+      if allowed_origins.include?(origin)
+        response.headers['Access-Control-Allow-Origin'] = origin
+      end
+      
       response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
       response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
     end
     head :ok
   end
@@ -27,10 +30,13 @@ class ApiController < ActionController::API
   def set_cors_headers
     allowed_origins = ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3001').split(',')
     origin = request.headers['Origin']
-
-    response.headers['Access-Control-Allow-Origin'] = origin if allowed_origins.include?(origin)
-
+    
+    if allowed_origins.include?(origin)
+      response.headers['Access-Control-Allow-Origin'] = origin
+    end
+    
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
   end
 end
