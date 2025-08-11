7f48e27fba86b6df845379f9b9fbb77e5dd8e308
danispinxo <genericpronoun@gmail.com>
2025-08-11 12:58:49 -0400

    some db fixes for the railway deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
   config.active_record.dump_schema_after_migration = false
 
-  config.active_record.database_url = ENV['DATABASE_URL'] if ENV['DATABASE_URL']
 
   config.action_cable.url = ENV.fetch('ACTION_CABLE_URL', nil)
   config.action_cable.allowed_request_origins = ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3001').split(',')
