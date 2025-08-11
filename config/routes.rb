08c9a7ed7ae8f2f0353406d9e28463a95420ea27
danispinxo <genericpronoun@gmail.com>
2025-08-11 13:40:36 -0400

    make route for backend active admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'up' => 'rails/health#show', as: :rails_health_check
+  
+  root 'admin/dashboard#index'
 
   namespace :api do
     devise_for :users, controllers: {
