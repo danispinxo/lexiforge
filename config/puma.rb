557e219d009ee631e9f522ce0b92eab791706695
danispinxo <genericpronoun@gmail.com>
2025-08-13 07:21:30 -0400

    standardize fetch format

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 2)
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
 threads min_threads_count, max_threads_count
 
 if ENV['RAILS_ENV'] == 'production'
   require 'concurrent-ruby'
-  worker_count = ENV.fetch('WEB_CONCURRENCY') { 2 }
+  worker_count = ENV.fetch('WEB_CONCURRENCY', 2)
   workers worker_count if worker_count > 1
 end
 
@@ -12,8 +12,8 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 port ENV.fetch('PORT', 3000)
 
-environment ENV.fetch('RAILS_ENV') { 'development' }
+environment ENV.fetch('RAILS_ENV', 'development')
 
-pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 plugin :tmp_restart
