1489226a4c7f4596cf9e8a998ceb22db0ad6e04b
danispinxo <genericpronoun@gmail.com>
2025-08-19 13:30:45 -0400

    set devise secret key base

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
 Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  
+  config.secret_key = Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
 
   require 'devise/orm/active_record'
 
