4210a8df19111d06e63dc0d15fcb505be1bd5530
danispinxo <genericpronoun@gmail.com>
2025-08-20 11:35:18 -0400

    info to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :admin
+  attributes :id, :email, :username, :first_name, :last_name, :bio, :full_name, :gravatar_url, :created_at, :admin
 
   def admin
     object.admin?
+  end
+
+  def full_name
+    object.full_name
+  end
+
+  def gravatar_url
+    object.gravatar_url
   end
 end
