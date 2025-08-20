e9987ed143e585e4174bc3689fc8ccd0d7eebd21
danispinxo <genericpronoun@gmail.com>
2025-08-20 12:27:15 -0400

    add poem attribution authorship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_20_155837) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_20_162057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_20_155837) do
     t.bigint "source_text_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author_type"
+    t.bigint "author_id"
+    t.index ["author_type", "author_id"], name: "index_poems_on_author"
     t.index ["source_text_id"], name: "index_poems_on_source_text_id"
   end
 
