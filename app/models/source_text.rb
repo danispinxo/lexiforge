e1181b87a1f227eaa870554393b59073fc2efe4b
danispinxo <genericpronoun@gmail.com>
2025-08-21 13:24:36 -0400

    add to source text model

--- app/models/source_text.rb
+++ app/models/source_text.rb
@@ -1,5 +1,6 @@
 class SourceText < ApplicationRecord
   has_many :poems, dependent: :destroy
+  belongs_to :user, optional: true
 
   validates :title, presence: true
   validates :content, presence: true
@@ -7,12 +8,15 @@ class SourceText < ApplicationRecord
 
   scope :from_gutenberg, -> { where.not(gutenberg_id: nil) }
   scope :custom, -> { where(gutenberg_id: nil) }
+  scope :public_texts, -> { where(is_public: true) }
+  scope :private_texts, -> { where(is_public: false) }
+  scope :for_user, ->(user) { where(user: user) }
 
   def self.ransackable_associations(_auth_object = nil)
     ['poems']
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[content created_at gutenberg_id id id_value title updated_at]
+    %w[content created_at gutenberg_id id id_value title updated_at is_public user_id]
   end
 end
