bd9b7ca99a41fd5b98b51ef66595073afed01b2d
danispinxo <genericpronoun@gmail.com>
2025-08-22 16:48:43 -0400

    Configure session store for cross-domain authentication with SameSite=None

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+Rails.application.config.session_store :cookie_store, 
+  key: '_lexiforge_session',
+  secure: Rails.env.production?, 
+  httponly: true,
+  same_site: :none,
+  expire_after: 2.weeks
