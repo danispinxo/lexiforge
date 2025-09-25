ba494752f0a93f77ed93243c4c896ba6f4c7a6c4
danispinxo <genericpronoun@gmail.com>
2025-09-25 12:40:00 -0400

    feat(ContentSecurityPolicy): enhance CSP configuration with additional sources and nonce generation

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,22 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data, 'fonts.googleapis.com', 'fonts.gstatic.com'
+    policy.img_src     :self, :https, :data, 'www.gravatar.com', 'gravatar.com'
+    policy.object_src  :none
+    policy.script_src  :self, :https
+    policy.style_src   :self, :https, :unsafe_inline, 'fonts.googleapis.com'
+    policy.connect_src :self, :https, *ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3001').split(',')
+
+    policy.frame_ancestors :self
+    policy.report_uri "/csp-violation-report-endpoint" if Rails.env.production?
+  end
+
+  config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
+  config.content_security_policy_nonce_directives = %w(script-src style-src)
+
+  config.content_security_policy_report_only = Rails.env.development?
+end
